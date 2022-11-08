f5d7905d3615d2a6ad3dea87462340406c35f295
Мария <mahadrum@yandex.ru>
2022-11-08 10:28:58 +0300

    Добавьте файлы проекта.

---
+++ Fiel/FormMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Fiel
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTemplate = new System.Windows.Forms.Label();
+            this.textBoxLetter = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTemplate
+            // 
+            this.labelTemplate.AutoSize = true;
+            this.labelTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTemplate.Location = new System.Drawing.Point(200, 67);
+            this.labelTemplate.Name = "labelTemplate";
+            this.labelTemplate.Size = new System.Drawing.Size(158, 55);
+            this.labelTemplate.TabIndex = 0;
+            this.labelTemplate.Text = "label1";
+            // 
+            // textBoxLetter
+            // 
+            this.textBoxLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLetter.Location = new System.Drawing.Point(157, 165);
+            this.textBoxLetter.Name = "textBoxLetter";
+            this.textBoxLetter.Size = new System.Drawing.Size(100, 80);
+            this.textBoxLetter.TabIndex = 1;
+            this.textBoxLetter.TextChanged += new System.EventHandler(this.textBoxLetter_TextChanged);
+            this.textBoxLetter.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxLetter_KeyDown);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Enabled = false;
+            this.buttonOk.Location = new System.Drawing.Point(157, 292);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(100, 23);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "проверить букву";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(394, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 153);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.textBoxLetter);
+            this.Controls.Add(this.labelTemplate);
+            this.Name = "FormMain";
+            this.Text = "Поле чудес";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTemplate;
+        private System.Windows.Forms.TextBox textBoxLetter;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Fiel/FormMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fiel
+{
+    
+    public partial class FormMain : Form
+    {
+        string word, template;
+        int error = 0;
+        public FormMain()
+        {
+            InitializeComponent();
+            word = "стул";
+            template = "****";
+            labelTemplate.Text = template;
+        }
+
+        private void textBoxLetter_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxLetter.Text.Length == 0)
+                buttonOk.Enabled = false;
+            else
+            {
+                buttonOk.Enabled = true;
+            }
+        }
+
+        private void textBoxLetter_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBoxLetter.Text = "";
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            /*
+            if (textBoxLetter.Text.Length == 0)
+            {
+                MessageBox.Show("введи букву, кретин!");
+                return;
+            }
+            */
+
+            char letter = textBoxLetter.Text[0];
+            string tempTemplate = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == letter)
+                    tempTemplate += letter;
+                else
+                    tempTemplate += template[i];
+            }
+            template = tempTemplate;
+            labelTemplate.Text = template;
+        }
+
+   
+    }
+}
